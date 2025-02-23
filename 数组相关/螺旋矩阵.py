87fec1fa80843b6b15c97bc9ee858785c005d23b
guruiqin <guruiqin@zhihu.com>
2025-02-23 12:53:27 +0800

    add

---
+++ 数组相关/螺旋矩阵.py
@@ -0,0 +1,50 @@
+# https://leetcode.cn/problems/spiral-matrix-ii/
+from pprint import pprint
+from typing import List
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+
+        startX = 0
+        startY = 0
+        offset = 1
+        count = 1
+
+        res = [[0] * n for _ in range(n)]
+        for _ in range(0, n // 2):
+
+            x, y = startX, startY
+
+            while y < n - offset:
+                res[x][y] = count
+                count += 1
+                y += 1
+            while x < n - offset:
+                res[x][y] = count
+                count += 1
+                x += 1
+            while y > startY:
+                res[x][y] = count
+                count += 1
+                y -= 1
+            while x > startX:
+                res[x][y] = count
+                count += 1
+                x -= 1
+
+            startX += 1
+            startY += 1
+            offset += 1
+
+
+        if n % 2 == 1:
+            res[n // 2][n // 2] = count
+
+        return res
+
+
+if __name__ == '__main__':
+    print(6 // 2)
+    res = Solution().generateMatrix(6)
+    pprint(res)
